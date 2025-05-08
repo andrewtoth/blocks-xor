4c3ea8ede8928e928c5e8d6a9c9a19f1ec6aa44b
Andrew Toth <andrewstoth@gmail.com>
2025-05-08 12:08:00 -0400

    use custom blocks dir from cli arg

--- README.md
+++ README.md
@@ -4,12 +4,14 @@ Bitcoin Core v28 and later versions have the ability to obfuscate the blocks dir
 However, this only takes effect if the blocks directory is fresh, i.e. you will have to resync the node from scratch.
 
 This program will obfuscate the blocks directory without having to redownload the blocks. It creates a random
-obfuscation key and XORs all block files. It currently only works for mainnet, and only if your blocks are stored in the
-default data directory.
+obfuscation key and XORs all block files. It only works for mainnet.
 
 ## Running
 
-Shut down bitcoind and run `cargo run --release`.
+Shut down bitcoind and run `cargo run --release`. This looks for blocks in the default location.
+If the blocks directory is in a different location, pass the location as the next argument:
+`cargo run --release ~/.bitcoin/blocks`.
+
 This can take some time, so please be patient.
 
 The program can be safely stopped and restarted until all files
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,55 @@
 use std::{
     env, fs,
     io::{self, Read},
+    path::Path,
     time::{Duration, Instant},
 };
 
 const MAGIC: [u8; 4] = [0xf9, 0xbe, 0xb4, 0xd9];
 
-fn main() {
+fn main() -> Result<(), io::Error> {
     let start = Instant::now();
 
-    let datadir = match env::consts::OS {
-        "macos" => "Library/Application Support/Bitcoin",
-        "windows" => "AppData\\Local\\Bitcoin",
-        "linux" => ".bitcoin",
-        _ => panic!("Unknown OS"),
+    let blocks_path = if env::args().len() > 1 {
+        env::args()
+            .skip(1)
+            .next()
+            .expect("the second arg to exist")
+            .into()
+    } else {
+        #[allow(deprecated)]
+        env::home_dir()
+            .expect("to have a home directory")
+            .join(match env::consts::OS {
+                "macos" => "Library/Application Support/Bitcoin",
+                "windows" => "AppData\\Local\\Bitcoin",
+                "linux" => ".bitcoin",
+                _ => {
+                    println!("Unknown OS");
+                    return Ok(());
+                }
+            })
+            .join("blocks")
     };
 
-    let blocks_path: std::path::PathBuf = env::home_dir().unwrap().join(datadir).join("blocks");
-
-    let paths = fs::read_dir(&blocks_path)
-        .unwrap()
+    let paths = fs::read_dir(&blocks_path)?
         .map(|res| res.map(|e| e.path()))
-        .collect::<Result<Vec<_>, io::Error>>()
-        .unwrap();
+        .collect::<Result<Vec<_>, io::Error>>()?;
 
     let xor_path: std::path::PathBuf = blocks_path.join("xor.dat");
-    if !fs::exists(&xor_path).unwrap() {
+    if !fs::exists(&xor_path)? {
         println!("No xor.dat file. Make sure you are running Bitcoin Core v28 or higher.");
-        return;
+        return Ok(());
     }
 
     println!("Xor'ing blocks dir. Do not start bitcoind until finished!");
 
-    let key: [u8; 8] = fs::read(&xor_path).unwrap().try_into().unwrap();
+    let key: [u8; 8] = fs::read(&xor_path)?
+        .try_into()
+        .expect("xor.dat to be 8 bytes");
     if key[0..4] == [0u8; 4] && key[4..] != [0u8; 4] {
         println!("This script doesn't work with a non-zero key with 4 bytes of leading zeros");
+        return Ok(());
     }
     let key = if key == [0u8; 8] {
         loop {
@@ -50,7 +65,7 @@ fn main() {
         key
     };
 
-    fs::write(xor_path, key).unwrap();
+    fs::write(xor_path, key)?;
 
     let total = paths.len();
     let mut done = 0;
@@ -58,39 +73,12 @@ fn main() {
     let duration = Duration::from_secs(5);
 
     paths.into_iter().for_each(|path| {
-        if !path.extension().is_some_and(|f| f == "dat") || path.iter().last().unwrap() == "xor.dat"
-        {
-            return;
-        }
-
-        let mut file = fs::File::open(&path).unwrap();
-        let mut buf = [0u8; 4];
-        if let Err(e) = file.read_exact(&mut buf) {
+        if let Err(e) = xor_block(&path, key) {
             println!(
-                "Error reading file {}: {e:?}",
-                path.iter().last().unwrap().to_str().unwrap()
-            );
-            return;
+                "Error xor-ing file {:?}: {e:?}",
+                path.iter().last().expect("path to have a last component")
+            )
         };
-
-        if buf == MAGIC {
-            let Ok(mut block) = fs::read(&path) else {
-                println!(
-                    "Error reading file {}",
-                    path.iter().last().unwrap().to_str().unwrap()
-                );
-                return;
-            };
-            block
-                .iter_mut()
-                .enumerate()
-                .for_each(|(i, b)| *b ^= key[i % key.len()]);
-
-            let mut tmp_path = path.as_os_str().to_owned();
-            tmp_path.push(".tmp");
-            fs::write(&tmp_path, block).unwrap();
-            fs::rename(&tmp_path, &path).unwrap();
-        }
 
         done += 1;
         if timer.elapsed() > duration {
@@ -103,4 +91,42 @@ fn main() {
         "Done in {} seconds! Blocksdir is now xor'd.",
         start.elapsed().as_secs()
     );
+
+    Ok(())
+}
+
+fn xor_block(path: &Path, key: [u8; 8]) -> Result<(), io::Error> {
+    if !path.extension().is_some_and(|f| f == "dat") {
+        return Ok(());
+    }
+
+    let file_name = path
+        .iter()
+        .last()
+        .expect("there to be a last path component");
+
+    if file_name == "xor.dat" {
+        return Ok(());
+    }
+
+    let mut file = fs::File::open(&path)?;
+    let mut buf = [0u8; 4];
+    file.read_exact(&mut buf)?;
+
+    if buf != MAGIC {
+        return Ok(());
+    }
+
+    let mut block = fs::read(&path)?;
+    block
+        .iter_mut()
+        .enumerate()
+        .for_each(|(i, b)| *b ^= key[i % key.len()]);
+
+    let mut tmp_path = path.as_os_str().to_owned();
+    tmp_path.push(".tmp");
+    fs::write(&tmp_path, block)?;
+    fs::rename(&tmp_path, &path)?;
+
+    Ok(())
 }
